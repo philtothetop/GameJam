290e9b13197110d0fe18967f35bf34cb625f4dce
Philippe Baron <Philippe Baron>
2016-06-11 17:10:27 -0400

    Created phases to delimit actions for each

--- GameProjekt/Assets/scripts/GameManager.cs
+++ GameProjekt/Assets/scripts/GameManager.cs
@@ -3,8 +3,31 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+
+    /*
+     * PHASE 1: START ROUND
+     * PHASE 2: PLACE YOUR PINS
+     * PHASE 3: RESULTS
+     */
+
+    enum phases { ROUNDSTART, PLACING, ACTION }
+    phases currentPhase;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+
+        assignGoals();
+        currentPhase = phases.ROUNDSTART;
+
+
+        // place la balle a 90 et 100 dans les y et 0 a 30 dans les X
+
+    }
+
+    // assigne 6 zones à chaque joueur
+    private static void assignGoals()
+    {
         GameObject[] goals = GameObject.FindGameObjectsWithTag("Goals");
         int playerOne = 0;
         int playerTwo = 0;
@@ -16,10 +39,13 @@ public class GameManager : MonoBehaviour {
             if (player == 1)
             {
 
-                if (playerOne >= 6) { 
+                if (playerOne >= 6)
+                {
                     player = 2;
                     playerTwo++;
-                } else { 
+                }
+                else
+                {
                     playerOne++;
                 }
             }
@@ -30,31 +56,30 @@ public class GameManager : MonoBehaviour {
                     player = 1;
                     playerOne++;
                 }
-                else { 
+                else
+                {
                     playerTwo++;
                 }
             }
 
-           
+
             goal.GetComponent<GoalManager>().player = player;
             goal.GetComponent<GoalManager>().setColor();
         }
 
         Debug.Log("Player 1 : " + playerOne + ", Player 2 : " + playerTwo);
-        // assigne 6 zones à chaque joueur
-
-        // place la balle a 90 et 100 dans les y et 0 a 30 dans les X
-
     }
-
-
-
-
-
-
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector3 mousePos;
+
+        // Create the 
+        if (currentPhase == phases.PLACING && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mousePos = Input.mousePosition;
+            Debug.Log("MOUSE CLICK AT: " + mousePos.ToString());
+        }
 	}
+
 }
--- GameProjekt/Assets/scripts/goalManager.cs
+++ GameProjekt/Assets/scripts/goalManager.cs
@@ -22,6 +22,7 @@ public class GoalManager : MonoBehaviour {
         }
     }
 
+
 	// Update is called once per frame
 	void Update () {
 	
