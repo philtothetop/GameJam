88a3d5d93f87360c2cb907505393a7797a449791
Samhain8 <fieryfredpolarix@gmail.com>
2016-06-11 17:59:12 -0400

    Added Pine Picker

---
+++ GameProjekt/Assets/scripts/PinePicker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts
+{
+    class PinePicker : MonoBehaviour
+    {
+        public Transform Pine;
+        void Update()
+        {
+            if (Pine == null) return; 
+
+            Pine.transform.position = UnityEngine.Input.mousePosition;
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                Pine.parent = transform;
+                Pine = null;
+            }
+        }
+    }
+}
