1edd04722da56514b17a9baeeb037ad59f0d7ce2
Samhain8 <fieryfredpolarix@gmail.com>
2016-06-11 22:55:44 -0400

    Stuff

--- GameProjekt/Assets/scripts/PineState.cs
+++ GameProjekt/Assets/scripts/PineState.cs
@@ -6,10 +6,10 @@ public class PineState : MonoBehaviour {
     // Use this for initialization
     public int breakPoints = 5;
     int test;
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
+    float time;
 	void Update () {
-	
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -18,7 +18,18 @@ public class PineState : MonoBehaviour {
         Debug.Log("POINTS LEFT: " + breakPoints);
         if (breakPoints == 0)
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
+        time = Time.unscaledTime;
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (time != 0 && Time.unscaledTime - 2 > time) Destroy(gameObject);
+    }
+
+    void OnCollisionExit(Collision2D coll)
+    {
+        time = 0;
     }
 }
