9786646fe7710cd2526c766f38e3cfde5f47618e
Philippe Baron <Philippe Baron>
2016-06-11 20:35:30 -0400

    Added score text

--- GameProjekt/Assets/scripts/GameManager.cs
+++ GameProjekt/Assets/scripts/GameManager.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-
-
     /*
      * PHASE 1: START ROUND
      * PHASE 2: PLACE YOUR PINS
@@ -13,16 +11,15 @@ public class GameManager : MonoBehaviour {
 
     enum phases { ROUNDSTART, PLACING, ACTION }
     phases currentPhase;
-
     public static int Player1Points;
     public static int Player2Points;
+    public Text scoreText;
 
     // Use this for initialization
     void Start ()
     {
-
         assignGoals();
-
+        updateScore();
         currentPhase = phases.ROUNDSTART;
         createBall();
 
@@ -42,7 +39,7 @@ public class GameManager : MonoBehaviour {
         float xValue = Random.Range(1f, 60f);
         GameObject ball = Instantiate(Resources.Load("Ball"), new Vector3(xValue, 95f), Quaternion.identity) as GameObject;
         // THIS SHOULD BE SET TO 1 WHEN GOING TO ACTION PHASE
-        ball.GetComponent<Rigidbody2D>().gravityScale = 0 ;
+        //ball.GetComponent<Rigidbody2D>().gravityScale = 0 ;
 
     }
 
@@ -59,7 +56,6 @@ public class GameManager : MonoBehaviour {
 
             if (player == 1)
             {
-
                 if (playerOne >= 6)
                 {
                     player = 2;
@@ -82,25 +78,27 @@ public class GameManager : MonoBehaviour {
                     playerTwo++;
                 }
             }
-
-
             goal.GetComponent<GoalManager>().player = player;
             goal.GetComponent<GoalManager>().setColor();
         }
-
         Debug.Log("Player 1 : " + playerOne + ", Player 2 : " + playerTwo);
+    }
+
+    public void updateScore()
+    {
+        scoreText.text = "Player 1: " + Player1Points + " | Player 2: " + Player2Points;
     }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 mousePos;
-
-        // Create the 
+        
         if (currentPhase == phases.PLACING && Input.GetKeyDown(KeyCode.Mouse0))
         {
             mousePos = Input.mousePosition;
             Debug.Log("MOUSE CLICK AT: " + mousePos.ToString());
         }
+        updateScore();
 	}
 
     
