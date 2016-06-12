b687cc640b840e8938a68f12b6c95787afa09bcc
Philippe Baron <Philippe Baron>
2016-06-11 22:03:34 -0400

    fixed the material bug

---
+++ GameProjekt/Assets/scripts/PineState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PineState : MonoBehaviour {
+
+    // Use this for initialization
+    public int breakPoints = 5;
+    int test;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        breakPoints--;
+        Debug.Log("POINTS LEFT: " + breakPoints);
+        if (breakPoints == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        Debug.Log("stuck " + test);
+        test++;
+    }
+}
